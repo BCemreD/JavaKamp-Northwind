1b8e6269d56e17e20cb44573f2197572a4e1fbf4
BCemreD <72667928+BCemreD@users.noreply.github.com>
2021-06-05 11:53:06 +0300

    9. Ders sonu

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,20 +5,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+//@Data(Getter ve Setter)
 @Entity
 @Table(name="products")
+//@AllArgsConstructor(parametreli consts)
+//@NoArgsConstructor(parametresiz constr.)
 public class Product {
 		@Id
 		@GeneratedValue(strategy = GenerationType.IDENTITY)
 		@Column(name="product_id")
 	private int id;
-		@Column(name="category_id")
-	private int categoryId;
+		//@Column(name="category_id")
+	//private int categoryId;
 		@Column(name="product_name")
 	private String productName;
 		@Column(name="unit_price")
@@ -27,6 +33,10 @@ public class Product {
 	private short unitsInStock;
 		@Column(name="quantity_per_unit")
 	private String quantityPerUnit;
+		
+		@ManyToOne()
+		@JoinColumn(name="category_id")
+	private Category category;
 
 	public Product () {}
 
@@ -34,7 +44,7 @@ public class Product {
 		String quantityPerUnit) {
 	super();
 	this.id = id;
-	this.categoryId = categoryId;
+	//this.categoryId = categoryId;
 	this.productName = productName;
 	this.unitPrice = unitPrice;
 	this.unitsInStock = unitsInStock;
@@ -49,13 +59,13 @@ public void setId(int id) {
 	this.id = id;
 }
 
-public int getCategoryId() {
+/*public int getCategoryId() {
 	return categoryId;
 }
 
 public void setCategoryId(int categoryId) {
 	this.categoryId = categoryId;
-}
+}*/
 
 public String getProductName() {
 	return productName;
